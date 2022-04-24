0503bc32f987b8722c1957269bc7632e84622cde
ashishmashal <ashishmashal5@gmail.com>
2022-04-24 12:14:42 +0530

    Added: Line Comparison Compare Method Is Added

--- src/com/bridgelabz/LineComparisonProblem/LIneComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem/LIneComparisonProblem.java
@@ -13,6 +13,16 @@ public class LIneComparisonProblem {
         else
             System.out.println("Lines are not equals...");
     }
+    //Method For Comparing Line
+    static void compare(Integer length1,Integer length2){
+        int result = length1.compareTo(length2);
+        if(result>0)
+            System.out.println("Line One is greater :");
+        else if(result<0)
+            System.out.println("Line two is greater : ");
+        else
+            System.out.println("Both lines are Equal...");
+    }
 
     public static void main(String[] args) {
 
@@ -50,6 +60,8 @@ public class LIneComparisonProblem {
 
         //Calling Equal Method
         equals(length1, length2);
+        //calling Compare Method
+        compare(length1, length2);
     }
 
 }
